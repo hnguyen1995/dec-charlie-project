cc46de8a0d8ada84dbccc64385cf591fbba1e0f4
hnguyen1995 <hnguyen1@una.edu>
2025-02-20 11:20:38 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  console.log("Welcome to the Past Projects Dashboard!");
-
-  // Easter Egg: Secret message in console
-  console.log("Easter Egg: You found the secret message! 🎉");
-
-  // Optional: You could add more interactivity here
-});
